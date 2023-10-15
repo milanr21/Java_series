a3375bebd3e23bdaefe597adf063ad4296b827bb
milanr21 <milanrawal309@gmail.com>
2023-10-15 16:17:02 +0545

    java

---
+++ Calculate.java
@@ -0,0 +1,10 @@
+class Calculate {
+    public static void main(String  args[]) {
+        float num1 = 6.5f;
+        boolean bol = true;
+
+    
+       
+        System.out.println(num1);
+    }
+}
